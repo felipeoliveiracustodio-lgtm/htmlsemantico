95d15ac52bfd7f31bfa6c5e5ad541c4c0cb557d8
felipeoliveiracustodio-lgtm <felipe.oliveira.custodio@escola.pr.gov.br>
2026-04-08 17:26:03 -0300

    Delete htmlsemantico directory

--- htmlsemantico/script.js
+++
@@ -1,9 +0,0 @@
-function alterarNome() {
-    let trocarNome = document.getElementById("inputNome").value;
-    if (trocarNome === '') {
-        alert('Digite um nome!')
-    
-    } else {
-        document.getElementById('nomeUsuario').innerHTML = trocarNome;
-    }
-}
